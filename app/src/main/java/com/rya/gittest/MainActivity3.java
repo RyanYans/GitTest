0a08cb18de573abdbd281e68cacb3bf95ddb9e4b
RyanYans <ryanyans32@gmail.com>
2016-08-13 19:08:56 +0800

    Version3.2.1

--- app/src/main/java/com/rya/gittest/MainActivity3.java
+++ app/src/main/java/com/rya/gittest/MainActivity3.java
@@ -10,7 +10,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "ryaHere!~", Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "ryaHere!~Good Show", Toast.LENGTH_LONG).show();
 
         System.out.println("Hello...Good World!~");
     }
